7889505837c727ddce50a4d770d1560ca512fc96
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-27 11:51:35 -0700

    Update
    Co-Authored-By: Filmer Tolentino <filmer.tolentino@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
