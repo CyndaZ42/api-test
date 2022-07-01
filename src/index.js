0783de147ecebc3229991111edd7fb75f09b0afe
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-01 16:52:27 -0700

    fetch update

--- src/index.js
+++ src/index.js
@@ -11,17 +11,41 @@ function clearFields() {
   $('.showTemp').text("");
 }
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
   });
 });
+
+
+
+
+//
+//$(document).ready(function() {
+//  $('#weatherLocation').click(function() {
+//    let city = $('#location').val();
+//    clearFields();
+//    let promise = WeatherService.getWeather(city);
+//    promise.then(function(response) {
+//      const body = JSON.parse(response);
+//      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//    }, function(error) {
+//      $('.showErrors').text(`There was an error processing your request: ${error}`);
+//    });
+//  });
+//});
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,17 +1,32 @@
 export default class WeatherService {  
   static getWeather(city) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
         }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
   }
 }
+
+//export default class WeatherService {  
+//  static getWeather(city) {
+//    return new Promise(function(resolve, reject) {
+//      let request = new XMLHttpRequest();
+//      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+//      request.onload = function() {
+//        if (this.status === 200) {
+//          resolve(request.response);
+//        } else {
+//          reject(request.response);
+//        }
+//      }
+//      request.open("GET", url, true);
+//      request.send();
+//    });
+//  }
+//}
